5d10dbd5d8e8e416dc2797abd1cb7d12c8600357
Juan Diego <juan97pena@gmail.com>
2024-11-05 16:53:48 +0100

    changed to compile with new tiago urdf

--- launch/move_group.launch.py
+++ launch/move_group.launch.py
@@ -13,103 +13,108 @@
 # limitations under the License.
 
 import os
-
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
-from launch.substitutions import LaunchConfiguration
 from launch_pal.arg_utils import read_launch_argument
-from launch_pal.robot_utils import (get_arm,
-                                    get_camera_model,
-                                    get_end_effector,
-                                    get_ft_sensor,
-                                    get_laser_model,
-                                    get_robot_name,
-                                    get_wrist_model)
 from launch_ros.actions import Node
-
-from moveit_configs_utils import MoveItConfigsBuilder
 from tiago_description.tiago_launch_utils import get_tiago_hw_suffix
-from launch_param_builder import ParameterBuilder
+from moveit_configs_utils import MoveItConfigsBuilder
+from launch_pal.arg_utils import LaunchArgumentsBase
+from tiago_description.launch_arguments import TiagoArgs
+from launch.substitutions import LaunchConfiguration
+from dataclasses import dataclass
+from launch_pal.robot_arguments import CommonArgs
+from ament_index_python.packages import get_package_share_directory
 
 
-def declare_args(context, *args, **kwargs):
+@dataclass(frozen=True)
+class LaunchArguments(LaunchArgumentsBase):
+    base_type: DeclareLaunchArgument = TiagoArgs.base_type
+    arm_type: DeclareLaunchArgument = TiagoArgs.arm_type
+    end_effector: DeclareLaunchArgument = TiagoArgs.end_effector
+    ft_sensor: DeclareLaunchArgument = TiagoArgs.ft_sensor
+    wrist_model: DeclareLaunchArgument = TiagoArgs.wrist_model
+    camera_model: DeclareLaunchArgument = TiagoArgs.camera_model
+    laser_model: DeclareLaunchArgument = TiagoArgs.laser_model
 
-    robot_name = read_launch_argument('robot_name', context)
+    use_sim_time: DeclareLaunchArgument = CommonArgs.use_sim_time
+    use_sensor_manager: DeclareLaunchArgument = CommonArgs.use_sensor_manager
 
-    # TIAGo description arguments
-    return [get_arm(robot_name),
-            get_camera_model(robot_name),
-            get_end_effector(robot_name),
-            get_ft_sensor(robot_name),
-            get_laser_model(robot_name),
-            get_wrist_model(robot_name)]
 
-def _octomap_launch_params(params: ParameterBuilder):
-    #params.yaml("config/sensors_kinect_pointcloud.yaml")
-    #params.yaml("config/sensors_3d.yaml")  # Done in MoveItConfigsBuilder instead.
-    params.parameter("octomap_frame", "camera_color_optical_frame")
-    params.parameter("octomap_resolution", 0.01)
-    params.parameter("max_range", 5.0)
-    return params.to_dict()
+def generate_launch_description():
 
-def launch_setup(context, *args, **kwargs):
+    # Create the launch description and populate
+    ld = LaunchDescription()
+    launch_arguments = LaunchArguments()
 
-    arm = read_launch_argument('arm', context)
-    camera_model = read_launch_argument('camera_model', context)
-    end_effector = read_launch_argument('end_effector', context)
-    ft_sensor = read_launch_argument('ft_sensor', context)
-    laser_model = read_launch_argument('laser_model', context)
-    wrist_model = read_launch_argument('wrist_model', context)
-    use_sensor_manager = read_launch_argument('use_sensor_manager', context)
+    launch_arguments.add_to_launch_description(ld)
 
-    robot_description_path = os.path.join(
-        get_package_share_directory('tiago_description'), 'robots', 'tiago.urdf.xacro')
+    declare_actions(ld, launch_arguments)
 
-    mappings = {
-        'arm': arm,
-        'camera_model': camera_model,
-        'end_effector': end_effector,
-        'ft_sensor': ft_sensor,
-        'laser_model': laser_model,
-        'wrist_model': wrist_model,
+    return ld
+
+
+def declare_actions(
+    launch_description: LaunchDescription, launch_args: LaunchArguments
+):
+
+    launch_description.add_action(OpaqueFunction(function=start_move_group))
+    return
+
+
+def start_move_group(context, *args, **kwargs):
+
+    base_type = read_launch_argument("base_type", context),
+    arm_type = read_launch_argument("arm_type", context)
+    end_effector = read_launch_argument("end_effector", context)
+    ft_sensor = read_launch_argument("ft_sensor", context)
+    use_sensor_manager = read_launch_argument("use_sensor_manager", context)
+
+    hw_suffix = get_tiago_hw_suffix(
+        arm=arm_type,
+        end_effector=end_effector,
+    )
+
+    srdf_file_path = os.path.join(
+        get_package_share_directory("tiago_moveit_config"),
+        "config", "srdf",
+        "tiago.srdf.xacro",
+    )
+
+    srdf_input_args = {
+        "arm_type": arm_type,
+        "end_effector": end_effector,
+        "ft_sensor": ft_sensor,
+        "base_type": base_type,
     }
-
-    robot_description_semantic = ('config/srdf/tiago' + get_tiago_hw_suffix(
-        arm=arm, end_effector=end_effector, ft_sensor=ft_sensor) + '.srdf')
 
     # Trajectory Execution Functionality
-    moveit_simple_controllers_path = (
-        'config/controllers/controllers' +
-        get_tiago_hw_suffix(arm=arm, end_effector=end_effector, ft_sensor=ft_sensor) + '.yaml')
+    moveit_simple_controllers_path = f"config/controllers/controllers{hw_suffix}.yaml"
+
+    robot_description_kinematics = "config/kinematics_kdl.yaml"
+    joint_limits = "config/joint_limits.yaml"
+    pilz_cartesian_limits = "config/pilz_cartesian_limits.yaml"
 
     planning_scene_monitor_parameters = {
-        'publish_planning_scene': True,
-        'publish_geometry_updates': True,
-        'publish_state_updates': True,
-        'publish_transforms_updates': True,
+        "publish_planning_scene": True,
+        "publish_geometry_updates": True,
+        "publish_state_updates": True,
+        "publish_transforms_updates": True,
+        "publish_robot_description": True,
     }
 
-    use_sim_time = {
-        'use_sim_time': LaunchConfiguration('use_sim_time')
-    }
-
-    _params_movegroup = ParameterBuilder("tiago_moveit_config")
-
-    # Load  ExecuteTaskSolutionCapability so we can execute found solutions in simulation
-    move_group_capabilities = {
-        "capabilities": "move_group/ExecuteTaskSolutionCapability"
-    }
-
+    # The robot description is read from the topic /robot_description if the parameter is empty
     moveit_config = (
-        MoveItConfigsBuilder('tiago')
-        .robot_description(file_path=robot_description_path, mappings=mappings)
-        .robot_description_semantic(file_path=robot_description_semantic)
-        .robot_description_kinematics(file_path=os.path.join('config', 'kinematics_kdl.yaml'))
+        MoveItConfigsBuilder("tiago")
+        .robot_description_semantic(file_path=srdf_file_path, mappings=srdf_input_args)
+        .robot_description_kinematics(file_path=robot_description_kinematics)
         .trajectory_execution(moveit_simple_controllers_path)
-        .planning_pipelines(pipelines=['ompl'])
         .planning_scene_monitor(planning_scene_monitor_parameters)
-        .pilz_cartesian_limits(file_path=os.path.join('config', 'pilz_cartesian_limits.yaml'))
+        .joint_limits(file_path=joint_limits)
+        .planning_pipelines(
+            pipelines=["ompl", "chomp"], default_planning_pipeline="ompl"
+        )
+        .pilz_cartesian_limits(file_path=pilz_cartesian_limits)
     )
 
     if use_sensor_manager:
@@ -122,44 +127,23 @@ def launch_setup(context, *args, **kwargs):
 
     moveit_config.to_moveit_configs()
 
+    move_group_configuration = {
+        "use_sim_time": LaunchConfiguration("use_sim_time"),
+        "publish_robot_description_semantic": True,
+    }
+
+    move_group_params = [
+        moveit_config.to_dict(),
+        move_group_configuration,
+    ]
+
     # Start the actual move_group node/action server
     run_move_group_node = Node(
-        package='moveit_ros_move_group',
-        executable='move_group',
-        output='screen',
-        parameters=[
-            use_sim_time,
-            moveit_config.to_dict(),
-            move_group_capabilities,
-            _octomap_launch_params(_params_movegroup)
-        ],
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        emulate_tty=True,
+        parameters=move_group_params,
     )
 
     return [run_move_group_node]
-
-
-def generate_launch_description():
-    # Command-line arguments
-    sim_time_arg = DeclareLaunchArgument(
-        'use_sim_time', default_value='False', description='Use sim time'
-    )
-
-    use_sensor_manager_arg = DeclareLaunchArgument(name='use_sensor_manager',
-                                                   default_value='False',
-                                                   choices=['True', 'False'],
-                                                   description='Use moveit_sensor_manager \
-                                                for octomap')
-
-    ld = LaunchDescription()
-
-    # Declare arguments
-    # we use OpaqueFunction so the callbacks have access to the context
-    ld.add_action(get_robot_name('tiago'))
-    ld.add_action(OpaqueFunction(function=declare_args))
-    ld.add_action(sim_time_arg)
-    ld.add_action(use_sensor_manager_arg)
-
-    # Execute move_group node
-    ld.add_action(OpaqueFunction(function=launch_setup))
-
-    return ld
